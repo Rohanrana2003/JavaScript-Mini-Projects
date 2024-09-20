e90d6a9c54657b3cf07c9209939b190ce02cd02e
Rohanrana2003 <rohanrana33042@gmail.com>
2024-09-20 23:46:57 +0530

    Final commit

--- Project - 1 Todo List (JavaScript)/script.js
+++ Project - 1 Todo List (JavaScript)/script.js
@@ -1,43 +1,43 @@
-const inputBox = document.getElementById('input-box')
-const listContainer = document.getElementById('list-container')
+const inputBox = document.getElementById('input-box');
+let listContainer = document.getElementById('list-container')
 
+function addTask() {
 
-const addTask = () =>{
-    if(inputBox.value === ""){
-        alert('You must write something!')
+    if (inputBox.value === '') {
+        alert('You must write something')
     }
-    else{
+
+    else {
         let li = document.createElement('li');
-        li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        li.innerText = inputBox.value;
         let span = document.createElement('span')
         span.innerHTML = '\u00d7'
         li.appendChild(span);
     }
-    inputBox.value ="";
+
+    inputBox.value='';
     saveData();
 }
 
-listContainer.addEventListener('click', (e)=>{
-    if(e.target.tagName === 'LI'){ 
-        e.target.classList.toggle('checked');
-        saveData();
-    }
-    else if(e.target.tagName === 'SPAN'){
-        e.target.parentElement.remove();
-        saveData();
-    }
-},false)
+    listContainer.addEventListener('click', (e)=>{
 
+        if(e.target.tagName === 'LI'){
+            e.target.classList.toggle('checked');
+            saveData();
+        }
+        else if(e.target.tagName === 'SPAN'){
+            e.target.parentElement.remove();
+            saveData();
+        }
+    })
 
 const saveData = () =>{
-    localStorage.setItem("data", listContainer.innerHTML);
+    localStorage.setItem('data', listContainer.innerHTML);
 }
 
-const showTask = () =>{
-    listContainer.innerHTML = localStorage.getItem("data");
+const showData = () =>{
+    listContainer.innerHTML =  localStorage.getItem('data');
 }
 
-showTask();
-
-
+showData();
